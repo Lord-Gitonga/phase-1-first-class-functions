510aaa104425e935c7a7100d14520e32ce0ff004
lordish <gitonga403@gmail.com>
2023-03-17 05:57:04 +0300

    Complete Assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function receivesAFunction (callBack) {
+    callBack();
+}
+
+function  returnsANamedFunction () {
+    function namedFunction (){
+
+    }
+    return namedFunction;
+}
+
+function returnsAnAnonymousFunction () {
+    return function () {
+
+    }
+}
